8f8d54e21f8e746ea0d4e3b7bdec359cc6d8102e
Taras <taras.peschenko.90@mail.ru>
2024-02-08 17:28:05 +0300

    add Tack1

---
+++ src/main/java/ru/gb/task1/Main.java
@@ -0,0 +1,2 @@
+package ru.gb.task1;public class Main {
+}
---
+++ src/main/java/ru/gb/task1/domen/Person.java
@@ -0,0 +1,42 @@
+package ru.gb.task1;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        return new EqualsBuilder().append(age, person.age).append(firstName, person.firstName).append(lastName, person.lastName).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(firstName)
+                .append(lastName)
+                .append(age)
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("firstName", firstName)
+                .append("lastName", lastName)
+                .append("age", age)
+                .toString();
+    }
+}
